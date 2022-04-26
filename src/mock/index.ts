cb6d6650760ed03f0fc1293d3e08aa97071fcc25
LinkunGao <gaolinkun123@gmail.com>
2022-04-26 15:25:41 +1200

    use ts to check vue data type

--- src/mock/index.ts
+++ src/mock/index.ts
@@ -1,13 +1,37 @@
 import Mock from "mockjs";
 
+const Requirement = {
+  WorkingYears: ["1年", "2年", "3年", "4年", "5年以上"],
+  Welfares: [
+    "五险一金",
+    "专业培训",
+    "免费班车",
+    "绩效奖金",
+    "员工旅游",
+    "年终奖金",
+  ],
+  PayMonths: ["12薪", "13薪", "14薪", "15薪"],
+  PayFilter: [
+    "3k以下",
+    "3-5k",
+    "5-10k",
+    "10-15k",
+    "15-20k",
+    "20-30k",
+    "30-50k",
+    "50k以上",
+  ],
+};
+
+// |5 表示自动生成5条数据
 Mock.mock("/mock/Job", "get", {
-  data: [
+  "data|5": [
     {
       jobName: "c#/.NET全栈工程师",
       payMin: "10",
       payMax: "15",
       payMonth: "15薪",
-      welfare: "五险一金，员工旅游，年终奖金，定期体检",
+      welfare: "五险一金, 员工旅游, 年终奖金, 定期体检",
       education: "本科",
       workingYears: "5年",
       publishTime: "2022-01-25T00:00:00",
@@ -17,3 +41,5 @@ Mock.mock("/mock/Job", "get", {
     },
   ],
 });
+
+Mock.mock("/mock/Requirement", "get", { data: Requirement });
--- src/views/index.vue
+++ src/views/index.vue
@@ -5,42 +5,88 @@
     </NavBar>
     <div class="selectBox">
       <div class="searchBox">
-        <input type="text" placeholder="实习生招聘" />
+        <input v-model="select.jobName" type="text" placeholder="实习生招聘" />
         <img src="../assets/search_icon.png" alt="" />
       </div>
       <div class="listBox">
         <div v-for="i in selectKey" :key="i.key" class="list">
           <span class="label">{{ i.name }}</span>
+          <span class="active">不限</span>
+          <span
+            @click="getJobList(option, i.inp)"
+            v-for="(option, index) in options[getKey(i)]"
+            :key="index"
+            >{{ option }}</span
+          >
         </div>
+      </div>
+    </div>
+    <div class="jobs-list">
+      <div v-for="job in jobs" :key="job.id" class="list">
+        <div class="left">
+          <span class="job-name">{{ job.jobName }}</span>
+          <span class="pay-num">{{ job.payMin }}K - {{ job.payMax }}K</span>
+          <span class="welfare-box">
+            <font
+              v-for="(welfare, index) in getWelfare(job.welfare)"
+              :key="index"
+            >
+              {{ welfare }}
+            </font>
+          </span>
+        </div>
+        <div>
+          <span>{{ job.workPosition }}</span>
+          <span>{{ job.publishTime }}</span>
+        </div>
+        <div class="btn">APPLY</div>
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, reactive } from "vue";
+import { defineComponent, onMounted, reactive, toRefs } from "vue";
 import NavBar from "@/components/NavBar.vue";
-import { getJob } from "../http/job";
+import { getJob, getRequirement } from "../http/job";
+import { selectKeyOptionInt, InitData } from "../types/index";
 
 export default defineComponent({
   components: { NavBar },
   //   setup 不支持异步
   setup() {
-    const data = reactive({
-      selectOptionList: {},
-    });
+    const data = reactive(new InitData());
     onMounted(() => {
       getJob({}).then((res: any) => {
+        data.jobs = res.data;
         console.log(res);
       });
+      getRequirement().then((res: any) => {
+        console.log(res.data);
+        data.options = res.data;
+      });
+      //   data.jobs[0].jobName
     });
-    const selectKey = [
-      { key: "WorkingYears", name: "工作经验" },
-      { key: "Welfares", name: "福利" },
-      { key: "PayMonths", name: "薪次" },
-      { key: "PayFilter", name: "薪资范围" },
+    const selectKey: Array<selectKeyOptionInt> = [
+      { key: "WorkingYears", name: "工作经验", inp: "education" },
+      { key: "Welfares", name: "福利", inp: "payarea" },
+      { key: "PayMonths", name: "薪次", inp: "payarea" },
+      { key: "PayFilter", name: "薪资范围", inp: "payarea" },
     ];
-    return { selectKey };
+
+    const getKey = (object: selectKeyOptionInt): keyof typeof data.options => {
+      const key: string = object.key;
+      return key as any;
+    };
+
+    const getWelfare = (str: string): string[] => {
+      return str.split(",");
+    };
+
+    const getJobList = (str: string, type: string): void => {
+      console.log(str);
+    };
+    return { ...toRefs(data), selectKey, getKey, getWelfare, getJobList };
   },
 });
 </script>
@@ -74,6 +120,67 @@ export default defineComponent({
   }
   img {
     height: 40px;
+    margin: 3px;
+  }
+}
+.listBox {
+  padding: 0 20px;
+  .list {
+    margin-top: 10px;
+    span {
+      margin-right: 15px;
+      cursor: pointer;
+      &.active {
+        color: cadetblue;
+      }
+    }
+    .label {
+      color: #999;
+      margin-right: 3px;
+    }
+  }
+}
+.jobs-list {
+  padding: 0 20px;
+  .list {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 40px 10px;
+    border-bottom: 1px solid #efefef;
+    font-size: 14px;
+    > div {
+      display: flex;
+      flex-direction: column;
+      &.btn {
+        display: inline-block;
+        text-align: center;
+        width: 70px;
+        background-color: cadetblue;
+        color: #fff;
+      }
+      span {
+        line-height: 28px;
+      }
+      .job-name {
+        font-weight: 600px;
+        font-size: 16px;
+      }
+      .pay-num {
+        font-weight: 400;
+        color: burlywood;
+      }
+      .welfare-box {
+        display: flex;
+        font {
+          background: #efefef;
+          padding: 0 5px;
+          font-size: 13px;
+          border-radius: 5px;
+          margin-right: 15px;
+        }
+      }
+    }
   }
 }
 </style>
